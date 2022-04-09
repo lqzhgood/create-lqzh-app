c8de2b18a8faf7d6428f36d5f32b22c0a0b1a059
lqzh <lqzhzc@outlook.com>
2022-04-09 13:18:05 +0800

    ✨ feat(添加模板化文件):

---
+++ tmpl/files.js
@@ -0,0 +1,3 @@
+// 需要模板化修改的文件
+
+module.exports = ['./package.json', './pm2.json'];
---
+++ tmpl/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'name',
+        message: '请输入项目名称',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: '请输入项目简介',
+    },
+];
