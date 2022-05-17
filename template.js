0a0582fef148ca9bb36d49ce07595bc2b0b6c4e6
lqzhgood <9134671+lqzhgood@users.noreply.github.com>
2022-05-17 16:20:44 +0800

    change repo name in templates

--- template.js
+++ template.js
@@ -2,8 +2,8 @@ const templates = [
     {
         key: 'nodejs',
         description: 'nodejs 模板',
-        url: 'https://github.com/lqzhgood/lqzh-create/tree/nodejs',
-        downloadUrl: 'https://github.com:lqzhgood/lqzh-create#nodejs',
+        url: 'https://github.com/lqzhgood/create-lqzh-app/tree/nodejs',
+        downloadUrl: 'https://github.com:lqzhgood/create-lqzh-app#nodejs',
         script: {
             post: 'npm install && npm run dev',
         },
