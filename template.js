3b6bad92869a728fd6ba0bd9ddd537c32ee4a482
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-07-29 11:41:04 +0800

    📃 docs(templete): change to full url

--- template.js
+++ template.js
@@ -3,7 +3,7 @@ const templates = [
         key: 'nodejs',
         description: 'nodejs 模板',
         url: 'https://github.com/lqzhgood/create-lqzh-app/tree/nodejs',
-        downloadUrl: 'https://github.com:lqzhgood/create-lqzh-app#nodejs',
+        downloadUrl: 'direct:https://github.com/lqzhgood/create-lqzh-app.git#nodejs',
         script: {
             post: 'npm install && npm run dev',
         },
