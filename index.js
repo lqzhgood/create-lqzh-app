47bcac2b481be548da72fc8dd36a08cab4a0f41e
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-12-02 00:13:20 +0800

    🦄 refactor(format): eslint prettier

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * @Description:lqzh
  * @Author: lqzh
  * @Date: 2022-04-09 10:52:32
- * @LastEditTime: 2022-04-09 11:13:38
+ * @LastEditTime: 2023-12-02 00:10:59
  */
 
 console.log('Hello Node');
+
+const a = 123;
+console.log('a', a);
