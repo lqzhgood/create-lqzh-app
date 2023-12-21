40ae93305efbb25a6ccd0bb91eae13ecc36bb3a2
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-12-21 22:41:31 +0800

    ✨ feat(ts): add ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description:lqzh
+ * @Author: lqzh
+ * @Date: 2022-04-09 10:52:32
+ * @LastEditTime: 2023-12-21 22:32:50
+ */
+
+console.log('Hello Node');
+
+const a: number = 123;
+console.log('a', a);
