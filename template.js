751d17f538236cc2717ec7180575368275f7b1f9
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-12-21 23:56:05 +0800

    🐞 fix(template): downloadUrl

--- template.js
+++ template.js
@@ -3,6 +3,8 @@ export default [
         key: 'nodejs',
         description: 'nodejs 模板',
         url: 'https://github.com/lqzhgood/create-lqzh-app/tree/nodejs',
-        downloadUrl: 'https://github.com/lqzhgood/create-lqzh-app.git#nodejs',
+        downloadUrl: 'github:lqzhgood/create-lqzh-app#nodejs',
+        // downloadUrl:
+        //     'direct:https://github.com/lqzhgood/create-lqzh-app.git#nodejs',
     },
 ];
